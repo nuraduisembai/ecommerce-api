2309badacc19469ea5431c65d81f1a04ce79647d
Nurassyl Duisembay <nuraduisembai@gmail.com>
2025-02-16 13:59:53 +0500

    Fix productRoutes.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
+require('dotenv').config();
 connectDB();
 
 const app = express();
 app.use(express.json());
 
+// Подключение маршрутов
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
